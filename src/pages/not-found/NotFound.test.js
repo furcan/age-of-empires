ab7e95bc05238f497b88364fd3550cd437a34529
Furkan MT <49978011+furcan@users.noreply.github.com>
2021-02-21 15:26:23 +0300

    Not found test changes.

--- src/pages/not-found/NotFound.test.js
+++ src/pages/not-found/NotFound.test.js
@@ -9,19 +9,3 @@ it('The NotFound component has been rendered as expected.', () => {
   const tree = create(<MemoryRouter><NotFound /></MemoryRouter>).toJSON();
   expect(tree).toMatchSnapshot();
 });
-
-describe('The NotFound Component => Go Back Button: ', () => {
-  test('It should go back when it has clicked.', () => {
-    let notFound;
-    act(() => {
-      notFound = create(<MemoryRouter><NotFound /></MemoryRouter>);
-    });
-
-    const testInstance = notFound.root;
-    const dummyHistory = createMemoryHistory({ initialEntries: ['/age-of-empires'] });
-    const goBackButton = testInstance.findByType('button');
-    act(() => goBackButton.props.onClick());
-
-    expect(dummyHistory.location.pathname).toBe('/age-of-empires');
-  });
-});
